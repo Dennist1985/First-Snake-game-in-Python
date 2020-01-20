205cc030a1adec2bba02687a66b4e88a1d1c6073
Dennis Timmerman <dennist1985@hotmail.nl>
2020-01-20 23:29:05 +0100

    A window is made with a red rectangle in center.

--- snake.py
+++ snake.py
@@ -1,17 +1,25 @@
 import pygame
 
 run = True
-pygame.display.init()
-screen_width = 500
-screen_height = 500
-# screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-screen = pygame.display.set_mode([screen_width, screen_height])
-pygame.display.toggle_fullscreen()
+pygame.init()
+screen_width = 1000
+screen_height = 1000
+snakeColor = (255,0,0)
 
+# The window is made.
+screen = pygame.display.set_mode([screen_width, screen_height])
+
+# The snake is drawn as a rectangle in center of window.
+snake = pygame.draw.rect(screen, snakeColor, (495, 495, 10, 10))
+
+# This is the a loop where the display keeps updating.
+# And to keep the window alive unless you close it yourself.
 while run == True:
-   for event in pygame.event.get():
+    for event in pygame.event.get():
        if event.type == pygame.QUIT:
            run = False
+    pygame.display.update()
+
 
 
 
